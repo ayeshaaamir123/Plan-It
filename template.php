c65171e7f8a336f3b52b8380f2061ed4fd94c4b5
ayeshaaamir123 <ayeshaaamir2001@gmail.com>
2022-01-27 00:40:45 +0400

    added error conditions template.php

--- template.php
+++ template.php
@@ -1,36 +1,55 @@
 <?php
      include 'partials/_dbconnect.php';
-
+ 
      if ($_SERVER["REQUEST_METHOD"] == 'POST' && $_POST['savechanges']=='pressed'){
+         //figure out: how to get templatetype from the button clicked and how to add alerts
+        // $templateType = $_POST['template'];
+        // echo $templateType;
+
+
          
-         // finding out whether the user already has any planners. If they do, add 1 to the number of planners and set that as the planner_id
-         $sql1= "SELECT * from `planit`.`planner` where email_id = 'ayeshaaamir2001@gmail.com';";
-         $result=mysqli_query($conn,$sql1);
-         $count = mysqli_num_rows($result);
-         $planner_id= $count+1;
-         $plannerName= $_POST['plannerName'];
-         $sql2= "INSERT INTO `planit`.`planner` (`planner_id`,`plannerName`, `templateType`, `email_id`) VALUES ('$planner_id', '$plannerName', 'business', 'ayeshaaamir2001@gmail.com');";
-         mysqli_query($conn,$sql2);
+        $taskcount=sizeof($_POST['taskName']);
+      
+        if($taskcount!=1 || ($_POST['taskName'][0]!='' && $_POST['taskDescription'][0]!='' && $_POST['startTask'][0]!= '' && $_POST['endTask'][0]!= '')){
+            // finding out whether the user already has any planners. If they do, add 1 to the number of planners and set that as the planner_id
+            $sql1= "SELECT * from `planit`.`planner` where email_id = 'ayeshaaamir2001@gmail.com';";
+            $result=mysqli_query($conn,$sql1);
+            $count = mysqli_num_rows($result);
+            $planner_id= $count+1;
+            $plannerName= $_POST['plannerName'];
+            $sql2= "INSERT INTO `planit`.`planner` (`planner_id`,`plannerName`, `templateType`, `email_id`) VALUES ('$planner_id', '$plannerName', 'business', 'ayeshaaamir2001@gmail.com');";
+            mysqli_query($conn,$sql2);
 
-         //adding tasks for the planner
-         $taskcount=sizeof($_POST['taskName']);
-
-         for ($x = 1; $x <=$taskcount; $x++) {
+            //adding tasks for the planner
+         
+            $y = 1;
+            for ($x = 1; $x <=$taskcount; $x++) {
              
+             
+                $taskName= $_POST['taskName'][$x-1];
+                $taskDescription= $_POST['taskDescription'][$x-1];
+                $startTask= $_POST['startTask'][$x-1];
+                $endTask= $_POST['endTask'][$x-1];
+             
+             
+                if ($taskName!='' && $taskDescription!='' && $startTask!='' && $endTask!=''){
 
-             $taskName= $_POST['taskName'][$x-1];
-             $taskDescription= $_POST['taskDescription'][$x-1];
-             $startTask= $_POST['startTask'][$x-1];
-             $endTask= $_POST['endTask'][$x-1];
-             $taskID= $x;
-
-
-             $sql3 = "INSERT INTO `planit`.`task` (`taskID`,`planner_id`, `email_id`, `taskName`, `taskDescription`, `startTask`, `endTask`) VALUES ('$taskID', '$planner_id', 'ayeshaaamir2001@gmail.com', '$taskName', '$taskDescription', '$startTask', '$endTask' );";
-             echo $sql3;
-             mysqli_query($conn,$sql3);
+                    $taskID= $y ;
+                    $sql3 = "INSERT INTO `planit`.`task` (`taskID`,`planner_id`, `email_id`, `taskName`, `taskDescription`, `startTask`, `endTask`) VALUES ('$taskID', '$planner_id', 'ayeshaaamir2001@gmail.com', '$taskName', '$taskDescription', '$startTask', '$endTask' );";
+                    mysqli_query($conn,$sql3);
+                    $y++;
+                }
+                else{
+                    echo 'TASK '.$x.' NOT ADDED';
+                }
+             
             }
+        }
+        else{
+            echo 'PLEASE ENTER COMPLETE DETAILS FOR THE TASK';
+        }
      }
-
+    
 ?>
 
 <!DOCTYPE html>
@@ -44,10 +63,12 @@
 
 <body>
 
+
 <?php require '<partials/_header.php';?>
 
+<!-- MODAL -->
 <div class="font-theme">
-      <div class="modal fade" id="Businessmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
 
                 <!-- Modal content-->
@@ -103,19 +124,23 @@
 
             </div>
         </div>
+
+<!-- Heading -->
     <div class="text-center font-theme">
         <h2 style="text-align:center;font-size:40px; color:#331766; padding-top: 1rem
     ;">
             Templates
         </h2>
     </div>
+
+<!-- Template Options -->
     <div class="container2">
-        <form action="" method="POST">
+        <form action="template.php" method="POST">
             <div class="card" style="width: 1000px;
 height: 100px;background:#F299F2BF; border-radius: 0; ">
                 <div class="card-body font-theme" style="font-size: 30px; ">
                     Business Planner
-                    <button type="button" name="submit" class="view" data-toggle="modal" data-target="#Businessmodal"></button>
+                    <button type="button" name="template" value="business" class="view" data-toggle="modal" data-target="#modal"></button>
                 </div>
             </div>
             <br>
@@ -123,7 +148,7 @@ height: 100px;background:#F299F2BF; border-radius: 0; ">
             height: 100px;background:#45D0BB; border-radius: 0; ">
                 <div class="card-body font-theme" style="font-size: 30px;  ">
                     Study Planner
-                    <button type="button" name="submit" class="view">Login</button>
+                    <button type="button" name="submit" class="view" data-toggle="modal" data-target="#modal"></button>
                 </div>
             </div>
             <br>
@@ -131,7 +156,7 @@ height: 100px;background:#F299F2BF; border-radius: 0; ">
                         height: 100px;background:#41C357CF; border-radius: 0; ">
                 <div class="card-body font-theme" style="font-size: 30px;  ">
                     Employee Planner
-                    <button type="button" name="submit" class="view">Login</button>
+                    <button type="button" name="submit" class="view" data-toggle="modal" data-target="#modal"></button>
                 </div>
             </div>
             <br>
@@ -139,7 +164,7 @@ height: 100px;background:#F299F2BF; border-radius: 0; ">
                                     height: 100px;background: #FFBF66DE; border-radius: 0; ">
                 <div class="card-body font-theme" style="font-size: 30px;  ">
                     Event Planner
-                    <button type="button" name="submit" class="view">Login</button>
+                    <button type="button" name="submit" class="view" data-toggle="modal" data-target="#modal"></button>
                 </div>
             </div>
             <br>
@@ -147,12 +172,7 @@ height: 100px;background:#F299F2BF; border-radius: 0; ">
     </div>
 </body>
 
-<!-- Showing modal through script -->
-<script>
-  $(document).ready(function() {
-    $("#Businessmodal").modal();
-  });
-</script>
+
 <script type="text/javascript">
     // Adding new task input fields as user clicks Add Task button
     $(document).ready(function() {
@@ -170,8 +190,6 @@ height: 100px;background:#F299F2BF; border-radius: 0; ">
             html += '<div class="input-group mb-3">';
             html+='<input type="text" name="endTask[]" class="form-control m-input" placeholder="Enter End Date" autocomplete="off"></div>';
             
-            
-
             $('#newTask').append(html);
 
             
